dfeaf405b0d79c6c313f66f47340a9dd8dd48256
Vincent Adam <vincent.adam87@gmail.com>
2013-12-31 20:26:28 +0000

    annotation of notebook, modification of parameter format

--- Code/exp_param.py
+++ Code/exp_param.py
@@ -7,6 +7,7 @@ class exp_param:
 	# Global parameters
 	rate = 44100 #44.1 khz, the sampling rate
 	f0 =250 
+	fi = f0*np.sqrt(2)
 	# Experiment structure
 	N_stim = 23 # total number of stim
 	N_rep = 1 # number of repetition per stim
@@ -32,34 +33,34 @@ class exp_param:
 
 	Sound_array = np.array([
 	    # ACT Stimuli
-	    [0,f0, 1, 0, DARK], #0
-	    [0,f0, 1, 0, BROAD], #1 
-	    [0,f0, 1, -6, BROAD], #2
-	    [0,f0, 1.69, 0, DARK], #3
-	    [0,f0, 1.69, 0, BROAD], #4
-	    [0,f0, 1.69, -6, BROAD], #5
-	    [0,f0, 2.25, 0, DARK], #6
-	    [0,f0, 2.25, 0, BROAD], #7
-	    [0,f0, 2.25, -6, BROAD], #8
-	    [0,f0, 2.89, 0, DARK], #9
-	    [0,f0, 2.89, 0, BROAD], #10
-	    [0,f0, 2.89, -6, BROAD], #11
-	    [0,f0, 4.0   , 0, DARK], #12
-	    [0,f0, 4.0   , 0, BROAD], #13
-	    [0,f0, 4.0   , -6, BROAD], #14
+	    [0, 0,f0, 1, DARK], #0
+	    [0, 0,f0, 1, BROAD], #1 
+	    [0, -6,f0, 1, BROAD], #2
+	    [0, 0,f0, 1.69, DARK], #3
+	    [0, 0,f0, 1.69, BROAD], #4
+	    [0, -6,f0, 1.69, BROAD], #5
+	    [0, 0,f0, 2.25, DARK], #6
+	    [0, 0,f0, 2.25, BROAD], #7
+	    [0, -6,f0, 2.25, BROAD], #8
+	    [0, 0,f0, 2.89, DARK], #9
+	    [0, 0,f0, 2.89, BROAD], #10
+	    [0, -6,f0, 2.89, BROAD], #11
+	    [0, 0,f0, 4.0   , DARK], #12
+	    [0, 0,f0, 4.0   , BROAD], #13
+	    [0, -6,f0, 4.0   , BROAD], #14
 	    # HCT Stimuli
-	    [1,f0, 0, DARK ], #15  % second is freq ratio (1 = f0, 0.5 = f0/2)
-	    [1,f0, 0, BROAD], #16
-	    [1,f0*2, 0, DARK], #17
-	    [1,f0*2, 0, BROAD], #18
+	    [1, 0,f0, DARK ], #15  % second is freq ratio (1 = f0, 0.5 = f0/2)
+	    [1, 0,f0, BROAD], #16
+	    [1, 0,f0*2, DARK], #17
+	    [1, 0,f0*2, BROAD], #18
 	    # ACT Control
-	    [2,f0, 4.0, 0, DARK], #19
-	    [2,f0, 4.0, 0, BROAD], #20 
-	    [2,2*f0, 4.0, -6, DARK], #21
-	    [2,2*f0, 4.0, -6, DARK], #22
+	    [2, 0,f0, 4.0, DARK], #19
+	    [2, 0,f0, 4.0, BROAD], #20 
+	    [2, -6,2*f0, 4.0, DARK], #21
+	    [2, -6,2*f0, 4.0, DARK], #22
 	    # Additional training
-	    [0,f0, 9   , 0, BROAD], #23
-   	    [0,f0, 9   , 0, DARK]]) #24
+	    [0, 0,f0, 9   , BROAD], #23
+   	    [0, 0,f0, 9   , DARK]]) #24
 
 	Calibration_sounds = [0,1,23,24]
 	Training_sounds1 = [0,1,23,24]
